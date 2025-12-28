f1705abbb509c4cd7a1390efa077222e7b877290
afzal.nandolia <afzal.nandolia@gmail.com>
2025-12-28 12:13:38 +0530

    initial commit

---
+++ backend/src/main/java/com/sitekit_backend/SiteKitApplication.java
@@ -0,0 +1,13 @@
+package com.sitekit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteKitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sitekit_backend/SiteKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitekit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
